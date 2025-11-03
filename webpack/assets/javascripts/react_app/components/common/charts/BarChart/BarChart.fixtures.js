33a3ac60ad04d3ffd1a2cb4ccd48afe051fac435
tlabaj <tlabaj@redhat.com>
2025-11-03 10:37:43 -0500

    Fixes #38879 - Update BarChart to Patternfly5

--- webpack/assets/javascripts/react_app/components/common/charts/BarChart/BarChart.fixtures.js
+++ webpack/assets/javascripts/react_app/components/common/charts/BarChart/BarChart.fixtures.js
@@ -47,3 +47,14 @@ export const barChartConfigWithEmptyData = {
 };
 
 export const emptyData = null;
+
+export const barChartDataWithColors = {
+  data: [
+    ['Fedora 21', 3, '#0088ce'],
+    ['Ubuntu 14.04', 4, '#ec7a08'],
+    ['Centos 7', 2, '#3f9c35'],
+    ['Debian 8', 1, '#005c66'],
+  ],
+  xAxisLabel: 'OS',
+  yAxisLabel: 'COUNT',
+};
--- webpack/assets/javascripts/react_app/components/common/charts/BarChart/index.js
+++ webpack/assets/javascripts/react_app/components/common/charts/BarChart/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { BarChart as PfBarChart } from 'patternfly-react';
-import { getBarChartConfig } from '../../../../../services/charts/BarChartService';
+import {
+  Chart,
+  ChartBar,
+  ChartAxis,
+  ChartGroup,
+  ChartThemeColor,
+} from '@patternfly/react-charts';
 import { noop } from '../../../../common/helpers';
 import { translate as __ } from '../../../../common/I18n';
 import MessageBox from '../../MessageBox';
+
+// Transform data for PatternFly 5 format
+const transformData = rawData => {
+  if (!rawData || !Array.isArray(rawData)) return [];
+
+  return rawData.map(item => ({
+    x: item[0], // label
+    y: item[1], // value
+    name: item[0],
+    color: item[2] || undefined, // custom color if provided
+  }));
+};
 
 const BarChart = ({
   data,
@@ -12,28 +29,84 @@ const BarChart = ({
   noDataMsg,
   config,
   title,
-  unloadData,
   xAxisLabel,
   yAxisLabel,
 }) => {
-  const chartConfig = getBarChartConfig({
-    data,
-    config,
-    onclick,
-    xAxisLabel,
-    yAxisLabel,
-  });
+  const chartData = transformData(data);
+  const hasCustomColors = chartData.some(d => d.color);
 
-  if (chartConfig.data.columns.length) {
-    return (
-      <PfBarChart
-        {...chartConfig}
-        title={title}
-        unloadBeforeLoad={unloadData}
-      />
-    );
+  // Handle empty data
+  if (!chartData.length) {
+    return <MessageBox msg={noDataMsg} icontype="info" />;
   }
-  return <MessageBox msg={noDataMsg} icontype="info" />;
+
+  // Configuration based on size (width is for viewBox aspect ratio, actual width is 100%)
+  const getChartDimensions = () => {
+    switch (config) {
+      case 'small':
+        return { height: 250, width: 600 };
+      case 'medium':
+        return { height: 280, width: 600 };
+      case 'regular':
+      default:
+        return { height: 300, width: 600 };
+    }
+  };
+
+  const dimensions = getChartDimensions();
+
+  // Handle click events
+  const handleBarClick = (event, clickedData) => {
+    if (onclick && typeof onclick === 'function') {
+      // Transform back to original format for compatibility
+      const originalData = [clickedData.name, clickedData.y];
+      onclick(originalData);
+    }
+  };
+
+  return (
+    <div style={{ height: dimensions.height + 60, width: '100%' }}>
+      <Chart
+        ariaDesc={title?.text || 'Bar chart'}
+        ariaTitle={title?.text || 'Bar chart'}
+        domainPadding={{ x: [30, 25] }}
+        height={dimensions.height}
+        width={dimensions.width}
+        themeColor={ChartThemeColor.multi}
+        padding={{
+          bottom: 60,
+          left: 70,
+          right: 20,
+          top: 10,
+        }}
+      >
+        <ChartAxis
+          dependentAxis
+          showGrid
+          label={yAxisLabel}
+          tickFormat={t => String(Math.round(t))}
+        />
+        <ChartAxis
+          label={xAxisLabel}
+          tickFormat={t => {
+            const str = String(t);
+            return str.length > 10 ? `${str.substring(0, 10)}...` : str;
+          }}
+        />
+        <ChartGroup>
+          <ChartBar
+            data={chartData}
+            onClick={handleBarClick}
+            style={
+              hasCustomColors
+                ? { data: { fill: ({ datum }) => datum.color } }
+                : undefined
+            }
+          />
+        </ChartGroup>
+      </Chart>
+    </div>
+  );
 };
 
 BarChart.propTypes = {
@@ -43,8 +116,8 @@ BarChart.propTypes = {
   config: PropTypes.string,
   title: PropTypes.shape({
     type: PropTypes.string,
+    text: PropTypes.string,
   }),
-  unloadData: PropTypes.bool,
   xAxisLabel: PropTypes.string,
   yAxisLabel: PropTypes.string,
 };
@@ -55,7 +128,6 @@ BarChart.defaultProps = {
   noDataMsg: __('No data available'),
   config: 'regular',
   title: { type: 'percent' },
-  unloadData: false,
   yAxisLabel: '',
   xAxisLabel: '',
 };
